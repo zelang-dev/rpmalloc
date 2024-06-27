de7ea282fd1ac082ac9292177d15fe3303406b23
Lawrence Stubbs <lstubbs@techno.express>
2024-06-26 20:21:33 -0400

    Suppress Windows x86 memory leak display
    - In Windows x86 builds, emulated tls, thread local storage has memory leak, need to figure out better clean up routine

--- rpmalloc/rpmalloc.c
+++ rpmalloc/rpmalloc.c
@@ -214,6 +214,10 @@ extern int madvise(caddr_t, size_t, int);
 #endif
 #include <errno.h>
 
+#if defined(ENABLE_ASSERTS) || defined(ENABLE_STATISTICS)
+#  include <stdio.h>
+#endif
+
 #if ENABLE_ASSERTS
 #  undef NDEBUG
 #  if defined(_MSC_VER) && !defined(_DEBUG)
@@ -235,9 +239,6 @@ extern int madvise(caddr_t, size_t, int);
 	} while (0)
 #else
 #  define rpmalloc_assert(truth, message) do {} while(0)
-#endif
-#if ENABLE_STATISTICS
-#  include <stdio.h>
 #endif
 
 //////
@@ -337,7 +338,11 @@ FORCEINLINE int rpmalloc_tls_set(tls_t key, void *val) {
 }
 
 void rpmalloc_shutdown(void) {
-    if (!rpmalloc_is_thread_initialized())
+    rp_free(rpmalloc_tls_get(_memory_thread_heap));
+    TlsFree(_memory_thread_heap);
+    rp_free(rpmalloc_tls_get(fls_key));
+    FlsFree(fls_key);
+    if (rpmalloc_is_thread_initialized())
         rpmalloc_finalize();
 }
 #else
